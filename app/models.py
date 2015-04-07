8ee160a52c14c06c35a20b227d7da869f56078a8
Taras Gordiyenko <taras.gordiyenko@gmail.com>
2015-04-07 13:47:40 -0400

    missed a primary key declaration

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class Score(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    initials = db.Column(db.String(3), primary_key=True)
+    initials = db.Column(db.String(3))
     score = db.Column(db.Integer())
 
     def __repr__(self):
